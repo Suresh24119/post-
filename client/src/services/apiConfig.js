ecb8669bf77262a336a4350227f9bd2b6dca206a
Suresh24119 <suresh24119@example.com>
2026-04-16 12:29:12 +0530

    live  backend url add now a forntend

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,2 +1,2 @@
-export const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+export const API_BASE = import.meta.env.VITE_API_BASE_URL || "https://post-seven-gamma.vercel.app";
 export const API_URL = `${API_BASE}/api`;
